4dd0a064359122fcac01cbde8416873445523695
Paolo Patierno <ppatierno@live.com>
2020-08-18 20:23:32 +0200

    Added skeleton for UDP-Kafka application

---
+++ f1-telemetry-udp-kafka/src/main/java/io/ppatierno/formula1/Application.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+public class Application {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
