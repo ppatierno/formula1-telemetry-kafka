9efa4b1fa23c09ab03205f4a3d3728eecec52c64
Paolo Patierno <ppatierno@live.com>
2020-08-26 15:18:55 +0200

    Added driver id as key for Driver Kafka messages

--- f1-telemetry-udp-kafka/src/main/java/io/ppatierno/formula1/DriversRouteBuilder.java
+++ f1-telemetry-udp-kafka/src/main/java/io/ppatierno/formula1/DriversRouteBuilder.java
@@ -5,6 +5,7 @@
 package io.ppatierno.formula1;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 
 public class DriversRouteBuilder extends RouteBuilder {
 
@@ -19,6 +20,10 @@ public class DriversRouteBuilder extends RouteBuilder {
         from("netty:udp://0.0.0.0:20777?decoders=#packet-decoder&sync=false")
                 .wireTap("direct:raw-packets")
                 .split().method("drivers-splitter", "splitDrivers")
+                .process(exchange -> {
+                    Driver driver = (Driver) exchange.getIn().getBody();
+                    exchange.getIn().setHeader(KafkaConstants.KEY, driver.getParticipantData().getDriverId().name());
+                })
                 .to("kafka:f1-telemetry-drivers?brokers=localhost:9092")
                 .routeId("udp-kafka-drivers")
                 .log("${body}");
