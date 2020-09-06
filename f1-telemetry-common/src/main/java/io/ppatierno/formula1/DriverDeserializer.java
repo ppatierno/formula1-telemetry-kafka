a52504178789564ee272dc87f3edafc081e69e4f
Paolo Patierno <ppatierno@live.com>
2020-09-06 15:59:27 +0200

    Added first drafts of Drive serializer/deserializer

---
+++ f1-telemetry-common/src/main/java/io/ppatierno/formula1/DriverDeserializer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.ppatierno.formula1.data.ParticipantData;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class DriverDeserializer implements Deserializer<Driver> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public Driver deserialize(String topic, byte[] bytes) {
+        ByteBuf bb = Unpooled.wrappedBuffer(bytes);
+        ParticipantData participantData = new ParticipantData();
+        participantData.fill(bb);
+        Driver driver = new Driver(participantData);
+        // TODO: deserialize other fields
+        return driver;
+    }
+
+    @Override
+    public Driver deserialize(String topic, Headers headers, byte[] data) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ f1-telemetry-common/src/main/java/io/ppatierno/formula1/DriverSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.ppatierno.formula1.data.ParticipantData;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class DriverSerializer implements Serializer<Driver> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public byte[] serialize(String topic, Driver driver) {
+        ByteBuf bb = Unpooled.buffer(ParticipantData.SIZE);
+        driver.getParticipantData().fillBuffer(bb);
+        // TODO: serialize other fields
+        return bb.array();
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, Driver data) {
+        return new byte[0];
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
