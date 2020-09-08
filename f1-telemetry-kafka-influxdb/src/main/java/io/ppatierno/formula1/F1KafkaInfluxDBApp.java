ef0bd2a6374dd40536c193e88a99bee6a1af357d
Paolo Patierno <ppatierno@live.com>
2020-09-08 22:30:07 +0200

    Created Kafka to InfluxDB project Added placeholder for Kafka to InfluxDB application

---
+++ f1-telemetry-kafka-influxdb/src/main/java/io/ppatierno/formula1/F1KafkaInfluxDBApp.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+public class F1KafkaInfluxDBApp {
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
