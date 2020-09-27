d0c68c1b667f1167ec1ba64eee9cfc50733dde0b
Paolo Patierno <ppatierno@live.com>
2020-09-27 20:37:40 +0200

    Added Event class for hosting event packet information Refactored route for Event(s) Updated dashboard with fastest lap and speed trap events

---
+++ f1-telemetry-common/src/main/java/io/ppatierno/formula1/EventDeserializer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.ppatierno.formula1.data.ParticipantData;
+import io.ppatierno.formula1.packets.PacketEventData;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class EventDeserializer implements Deserializer<Event> {
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public Event deserialize(String topic, byte[] bytes) {
+        return deserialize(topic, null, bytes);
+    }
+
+    @Override
+    public Event deserialize(String topic, Headers headers, byte[] bytes) {
+        ByteBuf bb = Unpooled.wrappedBuffer(bytes);
+        ParticipantData participantData = null;
+        // check if the raw bytes contains the participant data, depending the kind of event to deserialize
+        if (bytes.length > PacketEventData.SIZE) {
+            participantData = new ParticipantData().fill(bb);
+        }
+        Event event = new Event(participantData, (PacketEventData) new PacketEventData().fill(bb));
+        return event;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ f1-telemetry-common/src/main/java/io/ppatierno/formula1/PacketSerializer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Paolo Patierno.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.ppatierno.formula1;
+
+import io.ppatierno.formula1.packets.Packet;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class PacketSerializer implements Serializer<Packet> {
+
+    PacketEncoder encoder = new PacketEncoder();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public byte[] serialize(String topic, Packet packet) {
+        return serialize(topic, null, packet);
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, Packet packet) {
+        return this.encoder.encode(packet).array();
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
